d54bc46f375423d81e30b65e435a08e6c9f38db1
정서현 <tjgus027@kakao.com>
2024-01-26 21:53:34 +0900

    init setting

---
+++ src/main/java/com/sppart/admin/SuperpositionAdminApplication.java
@@ -0,0 +1,13 @@
+package com.sppart.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuperpositionAdminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SuperpositionAdminApplication.class, args);
+    }
+
+}

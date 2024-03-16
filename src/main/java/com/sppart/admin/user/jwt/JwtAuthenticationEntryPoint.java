fb7bbfd43a77ccf13e201dbb1ce2b9305cdbafd6
harrie8 <hesjs5@naver.com>
2024-03-16 13:16:11 +0900

    Feat: JWT 토큰 예외처리부에 로그 추가

--- src/main/java/com/sppart/admin/user/jwt/JwtAuthenticationEntryPoint.java
+++ src/main/java/com/sppart/admin/user/jwt/JwtAuthenticationEntryPoint.java
@@ -4,15 +4,18 @@ import java.io.IOException;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.stereotype.Component;
 
+@Slf4j
 @Component
 public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint {
     @Override
     public void commence(HttpServletRequest request, HttpServletResponse response,
                          AuthenticationException authException) throws IOException, ServletException {
+        log.info("인증이 필요합니다.");
         response.sendError(HttpServletResponse.SC_UNAUTHORIZED);
     }
 }

db14304de1abb7c27f92225a3d159c3d5868b62c
harrie8 <hesjs5@naver.com>
2024-03-16 13:17:48 +0900

    Feat: 게스트 권한 추가

--- src/main/java/com/sppart/admin/utils/Authority.java
+++ src/main/java/com/sppart/admin/utils/Authority.java
@@ -1,5 +1,14 @@
 package com.sppart.admin.utils;
 
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
 public enum Authority {
-    ROLE_ADMIN, ROLE_MANAGER;
+    ROLE_ADMIN("관리자"),
+    ROLE_MANAGER("매니저"),
+    ROLE_GUEST("게스트");
+
+    private final String value;
 }

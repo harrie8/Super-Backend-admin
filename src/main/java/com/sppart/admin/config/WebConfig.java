683c7e8b57d31312bf1482c30867e35a4a741035
harrie8 <hesjs5@naver.com>
2024-04-10 17:01:37 +0900

    Feat: Cors 설정 추가

--- src/main/java/com/sppart/admin/config/WebConfig.java
+++ src/main/java/com/sppart/admin/config/WebConfig.java
@@ -3,20 +3,35 @@ package com.sppart.admin.config;
 import com.sppart.admin.user.resolver.SessionArgumentResolver;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @RequiredArgsConstructor
 public class WebConfig implements WebMvcConfigurer {
-
-    // todo cors 설정 추가하기
+    @Value("${cors.pathPattern}")
+    private String pathPattern;
+    @Value("${cors.allowedOrigins}")
+    private String[] allowedOrigins;
 
     private final SessionArgumentResolver jwtArgumentResolver;
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping(pathPattern)
+                .allowedOrigins(allowedOrigins)
+                .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE")
+                .allowedHeaders("Authorization", "Content-Type")
+                .exposedHeaders("Custom-Header")
+                .allowCredentials(true)
+                .maxAge(3600);
+    }
 
     @Bean
     public PasswordEncoder passwordEncoder() {

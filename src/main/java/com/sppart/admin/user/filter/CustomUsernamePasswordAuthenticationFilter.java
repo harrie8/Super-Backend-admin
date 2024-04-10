42a7067b75f721906e8cce9c187350361f5c74d5
harrie8 <hesjs5@naver.com>
2024-04-10 17:21:23 +0900

    Refactor: 세션 검증 if 조건문 함수화 및 필터 거치지 않는 path 수정

--- src/main/java/com/sppart/admin/user/filter/CustomUsernamePasswordAuthenticationFilter.java
+++ src/main/java/com/sppart/admin/user/filter/CustomUsernamePasswordAuthenticationFilter.java
@@ -25,8 +25,7 @@ public class CustomUsernamePasswordAuthenticationFilter extends OncePerRequestFi
 
     @Override
     protected boolean shouldNotFilter(HttpServletRequest request) throws ServletException {
-        String[] excludePath = {"/user/signup", "/users/login", "/user/reissue", "/swagger-ui", "/v3/api-docs",
-                "/swagger-resources"};
+        String[] excludePath = {"/users/login", "/swagger-ui", "/v3/api-docs", "/swagger-resources"};
         String path = request.getRequestURI();
         return Arrays.stream(excludePath).anyMatch(path::startsWith);
     }
@@ -36,18 +35,19 @@ public class CustomUsernamePasswordAuthenticationFilter extends OncePerRequestFi
             throws ServletException, IOException {
 
         HttpSession session = request.getSession(false);
-        if (session == null) {
-            log.error("세션이 비어있는 오류");
+        if (isEmptySession(session)) {
+            log.error("세션이 비어 있는 오류");
             sendErrorResponse(response, 400, "세션이 비어 있습니다.");
             return;
         }
-        if (!(session.getAttribute(SessionConst.LOGIN_USER) instanceof Accessor accessor)) {
-            log.error("세션에 Accessor 객체가 아닌 {} 값이 설정되어 있음", session.getAttribute(SessionConst.LOGIN_USER));
-            sendErrorResponse(response, 400, "세션에 잘못된 값이 설정되어 있습니다.");
+        Object sessionAttribute = getSessionAttribute(session);
+        if (!(sessionAttribute instanceof Accessor accessor)) {
+            log.error("세션에 Accessor 객체가 아닌 {} 값이 설정되어 있음", sessionAttribute);
+            sendErrorResponse(response, 400, "세션에 " + sessionAttribute + " 값이 설정되어 있습니다.");
             return;
         }
 
-        UserDetails principal = new User(accessor.getId(), "", accessor.getRole().getGrantedAuthorities());
+        UserDetails principal = new User(accessor.getId(), "", accessor.getGrantedAuthorities());
         UsernamePasswordAuthenticationToken usernamePasswordAuthenticationToken = new UsernamePasswordAuthenticationToken(
                 principal, "", accessor.getRole().getGrantedAuthorities());
         SecurityContextHolder.getContext().setAuthentication(usernamePasswordAuthenticationToken);
@@ -57,5 +57,13 @@ public class CustomUsernamePasswordAuthenticationFilter extends OncePerRequestFi
 
     private void sendErrorResponse(HttpServletResponse response, int sc, String msg) throws IOException {
         response.sendError(sc, msg);
+    }
+
+    private boolean isEmptySession(HttpSession session) {
+        return session == null;
+    }
+
+    private Object getSessionAttribute(HttpSession session) {
+        return session.getAttribute(SessionConst.LOGIN_USER);
     }
 }

383bea99deb9e897aedabf3c0d371cd5e7933fd1
정서현 <tjgus027@kakao.com>
2024-05-11 22:27:40 +0900

    Fix&Refactor(Member): 트랜잭션 적용 및 이미지 업로드 메서드 활용

--- src/main/java/com/sppart/admin/member/service/MemberServiceImpl.java
+++ src/main/java/com/sppart/admin/member/service/MemberServiceImpl.java
@@ -1,16 +1,13 @@
 package com.sppart.admin.member.service;
 
 import com.amazonaws.services.s3.AmazonS3;
-import com.amazonaws.services.s3.model.CannedAccessControlList;
-import com.amazonaws.services.s3.model.ObjectMetadata;
-import com.amazonaws.services.s3.model.PutObjectRequest;
-import com.sppart.admin.exception.CommonErrorCode;
 import com.sppart.admin.member.domain.entity.Member;
 import com.sppart.admin.member.domain.mapper.MemberMapper;
 import com.sppart.admin.member.dto.Members;
 import com.sppart.admin.member.dto.ResponseMemberDetail;
 import com.sppart.admin.member.dto.ResponseMembers;
 import com.sppart.admin.member.dto.UpdateUserInfo;
+import com.sppart.admin.objectstorage.service.ObjectStorageService;
 import com.sppart.admin.utils.FilterType;
 import com.sppart.admin.utils.Page;
 import lombok.RequiredArgsConstructor;
@@ -20,10 +17,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.multipart.MultipartFile;
 
-import java.io.ByteArrayInputStream;
-import java.io.IOException;
 import java.util.*;
 
 @Slf4j
@@ -35,6 +29,7 @@ public class MemberServiceImpl implements MemberService{
 
     private final MemberMapper memberMapper;
     private final AmazonS3 awsS3Client;
+    private final ObjectStorageService objectStorageService;
 
     @Override
     @Transactional
@@ -90,7 +85,7 @@ public class MemberServiceImpl implements MemberService{
         }
 
         if (Objects.nonNull(userInfo.getImage())) {
-            String profileName = uploadProfileToStorage(userInfo.getImage());
+            String profileName = objectStorageService.uploadFile(userInfo.getImage());
             memberMapper.updateUserProfileByEmail(userEmail, profileName);
         }
 
@@ -154,43 +149,53 @@ public class MemberServiceImpl implements MemberService{
         }
     }
 
+    @Transactional
     public List<Members> getArtistMembers(int[] pageIndex, boolean isActiveMember){
         return entityToResponse(memberMapper.getArtisMember(pageIndex[0], pageIndex[1], isActiveMember));
     }
 
+    @Transactional
     public List<Members> getArtistMembersByEmail(String searchString, int[] pageIndex, boolean isActiveMember){
         return entityToResponse(memberMapper.getArtistMembersByEmail(searchString, pageIndex[0], pageIndex[1], isActiveMember));
     }
 
+    @Transactional
     public List<Members> getArtistMembersByName(String searchString, int[] pageIndex, boolean isActiveMember){
         return entityToResponse(memberMapper.getArtistMembersByName(searchString, pageIndex[0], pageIndex[1], isActiveMember));
     }
 
+    @Transactional
     public List<Members> getMembersByDate(Date startDate, Date endDate, int[] pageIndex, boolean isActiveMember) {
         return entityToResponse(memberMapper.getMembersByDate(startDate, endDate, pageIndex[0], pageIndex[1], isActiveMember));
     }
 
-    private List<Members> getMembersByDateAndEmail(String searchString, Date startDate, Date endDate, int[] pageIndex, boolean isActiveMember) {
+    @Transactional
+    public List<Members> getMembersByDateAndEmail(String searchString, Date startDate, Date endDate, int[] pageIndex, boolean isActiveMember) {
         return entityToResponse(memberMapper.getMembersByDateAndEmail(searchString, startDate, endDate, pageIndex[0], pageIndex[1], isActiveMember));
     }
 
+    @Transactional
     public List<Members> getMembersByDateAndName(String searchString, Date startDate, Date endDate, int[] pageIndex, boolean isActiveMember) {
         return entityToResponse(memberMapper.getMembersByDateAndName(searchString, startDate, endDate, pageIndex[0], pageIndex[1], isActiveMember));
     }
 
+    @Transactional
     public List<Members> getArtistMembersByDate(Date startDate, Date endDate, int[] pageIndex, boolean isActiveMember) {
         return entityToResponse(memberMapper.getArtistMembersByDate(startDate, endDate, pageIndex[0], pageIndex[1], isActiveMember));
     }
 
+    @Transactional
     public List<Members> getArtistMembersByDateAndEmail(String searchString, Date startDate, Date endDate, int[] pageIndex, boolean isActiveMember) {
          return entityToResponse(memberMapper.getArtistMembersByDateAndEmail(searchString, startDate, endDate, pageIndex[0], pageIndex[1], isActiveMember));
     }
 
+    @Transactional
     public List<Members> getArtistMembersByDateAndName(String searchString, Date startDate, Date endDate, int[] pageIndex, boolean isActiveMember) {
          return entityToResponse(memberMapper.getArtistMembersByDateAndName(searchString, startDate, endDate, pageIndex[0], pageIndex[1], isActiveMember));
     }
 
     //TODO : 비동기 동작하도록 구현 필요
+    @Transactional(propagation = Propagation.NOT_SUPPORTED)
     public List<Members> entityToResponse(List<Member> members){
         List<Members> memberList = new ArrayList<>(members.size());
         members.forEach(member -> memberList.add(
@@ -202,36 +207,5 @@ public class MemberServiceImpl implements MemberService{
                             .createAt(member.getCreateAt())
                             .updateAt(member.getNicknameUpdateAt()).build()));
         return memberList;
-    }
-
-    @Transactional(propagation = Propagation.NOT_SUPPORTED)
-    public String uploadProfileToStorage(MultipartFile file) {
-        if(file.isEmpty()) {
-            return null;
-        } else {
-            String fileName = file.getOriginalFilename();
-            String extension = fileName != null ? fileName.substring(fileName.lastIndexOf(".")) : null;
-            String profile = setFileName(extension);
-
-            try {
-                byte[] fileData = file.getBytes();
-                ObjectMetadata data = new ObjectMetadata();
-                data.setContentLength(fileData.length);
-                data.setContentDisposition("inline; filename=" + profile);
-
-                awsS3Client.putObject(new PutObjectRequest(
-                        bucketName, profile,
-                        new ByteArrayInputStream(fileData), data)
-                        .withCannedAcl(CannedAccessControlList.PublicRead));
-                return profile;
-            } catch (IOException e) {
-                throw new RuntimeException(CommonErrorCode.INTERNAL_SERVER_ERROR.getMessage());
-            }
-        }
-    }
-
-    private String setFileName(String extension){
-        if (extension == null) return UUID.randomUUID().toString();
-        return UUID.randomUUID() + extension;
     }
 }

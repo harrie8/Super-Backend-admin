c72e1ff2a81542430f79de13f1ceb0be647bc5af
harrie8 <hesjs5@naver.com>
2024-05-29 23:49:39 +0900

    Fix: RequestMapping 추가

--- src/main/java/com/sppart/admin/artist/controller/ArtistController.java
+++ src/main/java/com/sppart/admin/artist/controller/ArtistController.java
@@ -4,26 +4,35 @@ import com.sppart.admin.artist.dto.RequestArtistInfo;
 import com.sppart.admin.artist.dto.RequestUpdateArtistInfo;
 import com.sppart.admin.artist.service.ArtistService;
 import com.sppart.admin.utils.FilterType;
+import java.util.Date;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Date;
-
+@RestController
 @RequiredArgsConstructor
-@RestController("/artists")
+@RequestMapping("/artists")
 public class ArtistController {
     private final ArtistService artistService;
 
     @GetMapping
     public ResponseEntity<?> getAllArtists(@RequestParam(value = "startDate", required = false) Date startDate,
-                             @RequestParam(value = "endDate", required = false) Date endDate,
-                             @RequestParam(value = "isUser", defaultValue = "false") boolean isUser,
-                             @RequestParam(value = "searchString", required = false) String searchString,
-                             @RequestParam(value = "filterType", defaultValue = "all") FilterType filterType,
-                             @RequestParam(value = "page", defaultValue = "1") int page) {
-        return ResponseEntity.ok(artistService.getAllArtists(startDate, endDate, isUser, searchString, filterType, page));
+                                           @RequestParam(value = "endDate", required = false) Date endDate,
+                                           @RequestParam(value = "isUser", defaultValue = "false") boolean isUser,
+                                           @RequestParam(value = "searchString", required = false) String searchString,
+                                           @RequestParam(value = "filterType", defaultValue = "all") FilterType filterType,
+                                           @RequestParam(value = "page", defaultValue = "1") int page) {
+        return ResponseEntity.ok(
+                artistService.getAllArtists(startDate, endDate, isUser, searchString, filterType, page));
     }
 
     @GetMapping("/one")
--- src/main/java/com/sppart/admin/member/controller/MemberController.java
+++ src/main/java/com/sppart/admin/member/controller/MemberController.java
@@ -3,15 +3,22 @@ package com.sppart.admin.member.controller;
 import com.sppart.admin.member.dto.UpdateUserInfo;
 import com.sppart.admin.member.service.MemberService;
 import com.sppart.admin.utils.FilterType;
+import java.util.Date;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
 
 @RequiredArgsConstructor
-@RestController("/members")
+@RestController
+@RequestMapping("/members")
 public class MemberController {
     private final MemberService memberService;
 
@@ -21,18 +28,20 @@ public class MemberController {
                                            @RequestParam(value = "isAuthor", defaultValue = "false") boolean isAuthor,
                                            @RequestParam(value = "searchString", required = false) String searchString,
                                            @RequestParam(value = "filterType", defaultValue = "all") FilterType filterType,
-                                           @RequestParam(value = "page", defaultValue = "1") int page){
-        return ResponseEntity.ok(memberService.getAllMembers(startDate, endDate, isAuthor, searchString, filterType, page));
+                                           @RequestParam(value = "page", defaultValue = "1") int page) {
+        return ResponseEntity.ok(
+                memberService.getAllMembers(startDate, endDate, isAuthor, searchString, filterType, page));
     }
 
     @GetMapping("/inactive")
     public ResponseEntity<?> getAllDeleteMembers(@RequestParam(value = "startDate", required = false) Date startDate,
-                                           @RequestParam(value = "endDate", required = false) Date endDate,
-                                           @RequestParam(value = "isAuthor", defaultValue = "false") boolean isAuthor,
-                                           @RequestParam(value = "searchString", required = false) String searchString,
-                                           @RequestParam(value = "filterType", defaultValue = "all") FilterType filterType,
-                                           @RequestParam(value = "page", defaultValue = "1") int page){
-        return ResponseEntity.ok(memberService.getDeleteMembers(startDate, endDate, isAuthor, searchString, filterType, page));
+                                                 @RequestParam(value = "endDate", required = false) Date endDate,
+                                                 @RequestParam(value = "isAuthor", defaultValue = "false") boolean isAuthor,
+                                                 @RequestParam(value = "searchString", required = false) String searchString,
+                                                 @RequestParam(value = "filterType", defaultValue = "all") FilterType filterType,
+                                                 @RequestParam(value = "page", defaultValue = "1") int page) {
+        return ResponseEntity.ok(
+                memberService.getDeleteMembers(startDate, endDate, isAuthor, searchString, filterType, page));
     }
 
     @GetMapping("/one")

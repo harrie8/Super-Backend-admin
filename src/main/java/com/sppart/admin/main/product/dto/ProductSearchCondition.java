f2bb7280054c45a2028a0b02bd5bd57bbe9339fb
harrie8 <hesjs5@naver.com>
2024-06-22 14:46:22 +0900

    Fix: 작품 목록 조회 기능에 페이징 추가

--- src/main/java/com/sppart/admin/main/product/dto/ProductSearchCondition.java
+++ src/main/java/com/sppart/admin/main/product/dto/ProductSearchCondition.java
@@ -3,19 +3,35 @@ package com.sppart.admin.main.product.dto;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.springframework.data.domain.Pageable;
 
 @Getter
 @NoArgsConstructor
 public class ProductSearchCondition {
 
+    private Pageable pageable;
     private String title;
     private String artistName;
     private Long productId; // 작품 코드
 
     @Builder
-    public ProductSearchCondition(String title, String artistName, Long productId) {
+    public ProductSearchCondition(Pageable pageable, String title, String artistName, Long productId) {
+        this.pageable = pageable;
         this.title = title;
         this.artistName = artistName;
         this.productId = productId;
+    }
+
+    public int getPageNumber() {
+        return pageable.getPageNumber();
+    }
+
+    public int getPageSize() {
+        return pageable.getPageSize();
+    }
+
+    // for mybatis
+    public int getOffset() {
+        return (pageable.getPageNumber() - 1) * pageable.getPageSize();
     }
 }
--- src/main/java/com/sppart/admin/main/product/service/ProductService.java
+++ src/main/java/com/sppart/admin/main/product/service/ProductService.java
@@ -1,18 +1,18 @@
 package com.sppart.admin.main.product.service;
 
+import com.sppart.admin.main.product.dto.DetailProductInfo;
 import com.sppart.admin.main.product.dto.ProductSearchCondition;
 import com.sppart.admin.main.product.dto.request.RequestCreateProduct;
 import com.sppart.admin.main.product.dto.request.RequestUpdateProduct;
 import com.sppart.admin.main.product.dto.response.ResponseBulkDeleteProductByIds;
 import com.sppart.admin.main.product.dto.response.ResponseDetailProductInfo;
-import com.sppart.admin.main.product.dto.response.ResponseGetProductsByCondition;
-import com.sppart.admin.main.product.dto.response.ResponsePaging;
+import com.sppart.admin.utils.PageInfo;
 import java.util.Set;
 import org.springframework.web.multipart.MultipartFile;
 
 public interface ProductService {
 
-    ResponsePaging<ResponseGetProductsByCondition> getProductsByCondition(ProductSearchCondition condition);
+    PageInfo<DetailProductInfo> getProductsByCondition(ProductSearchCondition condition);
 
     ResponseBulkDeleteProductByIds bulkDeleteByIds(Set<Long> ids);
 
